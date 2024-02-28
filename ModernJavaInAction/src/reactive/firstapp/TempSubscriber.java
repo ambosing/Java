bec29bc1ad999cf74356137648f76483183792c9
ambosing <ambosing_@naver.com>
2024-02-28 23:40:30 +0900

    Reactive Flow API 사용해보기

---
+++ ModernJavaInAction/src/reactive/firstapp/TempSubscriber.java
@@ -0,0 +1,34 @@
+package reactive.firstapp;
+
+import java.util.concurrent.Flow;
+import java.util.concurrent.Flow.Subscriber;
+
+import static java.util.concurrent.Flow.*;
+
+public class TempSubscriber implements Subscriber<TempInfo> {
+
+    private Subscription subscription;
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1);
+    }
+
+    @Override
+    public void onNext(TempInfo item) {
+        System.out.println(item);
+
+        subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        System.err.println(throwable.getMessage());
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("Done!");
+    }
+}
