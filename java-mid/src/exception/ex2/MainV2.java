85e39f8ae1741599e42ddd3bc6995c65b8c4042a
ambosing <ambosing_@naver.com>
2024-04-29 13:34:00 +0900

    예외 처리 리소스 반환 문제

--- java-mid/src/exception/ex2/MainV2.java
+++ java-mid/src/exception/ex2/MainV2.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class MainV2 {
 	public static void main(String[] args) throws NetworkClientExceptionV2 {
-		NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
+		// NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
+		// NetworkServiceV2_4 networkService = new NetworkServiceV2_4();
+		NetworkServiceV2_5 networkService = new NetworkServiceV2_5();
 
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
