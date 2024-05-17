372f625f0e329a927d39fb590343fe3e837f7c71
ambosing <ambosing_@naver.com>
2024-05-17 14:15:53 +0900

    arrayList 기능 추가

--- java-mid2/src/collection/array/MyArrayListV3.java
+++ java-mid2/src/collection/array/MyArrayListV3.java
@@ -21,7 +21,6 @@ public class MyArrayListV3 {
 	}
 
 	public void add(Object e) {
-		// 코드 추가
 		if (size == elementData.length) {
 			grow();
 		}
@@ -29,7 +28,23 @@ public class MyArrayListV3 {
 		size++;
 	}
 
-	// 코드 추가
+	public void add(int index, Object e) {
+		if (size == elementData.length) {
+			grow();
+		}
+		// 데이터 이동
+		shiftRightFrom(index);
+		elementData[index] = e;
+		size++;
+	}
+
+	// 코드 추가, 요소의 마지막부터 index까지 오른쪽으로 밀기
+	private void shiftRightFrom(int index) {
+		for (int i = size; i > index; i--) {
+			elementData[i] = elementData[i - 1];
+		}
+	}
+
 	private void grow() {
 		int oldCapacity = elementData.length;
 		int newCapacity = oldCapacity * 2;
@@ -46,6 +61,23 @@ public class MyArrayListV3 {
 		Object oldValue = get(index);
 		elementData[index] = element;
 		return oldValue;
+	}
+
+	// 코드 추가
+	public Object remove(int index) {
+		Object oldValue = get(index);
+		shiftLeftFrom(index);
+
+		size--;
+		elementData[size] = null;
+		return oldValue;
+	}
+
+	// 코드 추가, 요소의 index부터 마지막까지 왼쪽으로 밀기
+	private void shiftLeftFrom(int index) {
+		for (int i = index; i < size - 1; i++) {
+			elementData[i] = elementData[i + 1];
+		}
 	}
 
 	public int indexOf(Object o) {
