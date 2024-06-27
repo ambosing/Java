f77f0ccc57554cc521b206a67f113d27fc4ccd20
ambosing <ambosing_@naver.com>
2024-06-27 11:43:53 +0900

    for-each의 비밀! Iterable과의 관계

--- java-mid2/src/collection/iterable/MyArrayMain.java
+++ java-mid2/src/collection/iterable/MyArrayMain.java
@@ -8,9 +8,14 @@ public class MyArrayMain {
 
 		System.out.println("iterator 사용");
 		Iterator<Integer> iterator = myArray.iterator();
-		
+
 		while (iterator.hasNext()) {
 			System.out.println(iterator.next());
+		}
+
+		System.out.println("for-each 사용 <- Iterable을 구현해야 함");
+		for (int value : myArray) {
+			System.out.println("value = " + value);
 		}
 	}
 }
